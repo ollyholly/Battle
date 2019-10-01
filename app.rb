fbee6dff46a1628120f097e64ab2f0ebd8e91739
Jessica Marais <jessica_marais@hotmail.com>
2019-10-01 16:46:20 +0100

    Add functionality for 2 palyers to enter name

--- app.rb
+++ app.rb
@@ -5,9 +5,14 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   get '/' do
-    "Testing infrastructure working!"
+    erb :index
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
   end
 
   run! if app_file == $0
 end
-  
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,8 +1,8 @@
-feature 'Testing infrastructure' do
-
-  scenario 'Can run app and check content' do
-    visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-
-end
+# feature 'Testing infrastructure' do
+#
+#   scenario 'Can run app and check content' do
+#     visit('/')
+#     expect(page).to have_content 'Testing infrastructure working!'
+#   end
+#
+# end
