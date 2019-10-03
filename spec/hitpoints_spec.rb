47543e7e511287a02d63e72c6715e6f3b3b0eb39
Olly Holovchenko <oleh.holo@gmail.com>
2019-10-03 16:29:18 +0100

    Fix bugs and move OK link to Attack page

--- spec/hitpoints_spec.rb
+++ spec/hitpoints_spec.rb
@@ -1,6 +1,6 @@
 feature 'Hit Points' do
   scenario 'Have starting HP' do
     sign_in_and_play
-    expect(page).to have_content 'Jess: HP 60'
+    expect(page).to have_content 'Olly 60 :HP'
   end
 end
